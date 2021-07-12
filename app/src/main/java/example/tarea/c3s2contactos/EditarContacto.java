0da91cd33b04c1324899010ed4cb680efcc4a698
Desencuentro <73045355+Desencuentro@users.noreply.github.com>
2021-07-12 04:06:06 -0300

    Plantilla actividad EditarContacto

---
+++ app/src/main/java/example/tarea/c3s2contactos/EditarContacto.java
@@ -0,0 +1,14 @@
+package example.tarea.c3s2contactos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarContacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacto_editar);
+    }
+}
